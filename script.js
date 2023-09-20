c27cd2f1c65444be5a4a9bc678fdbb67d8bf61fb
KnotEnvy <jwsnyder@gmail.com>
2023-09-20 12:16:14 -0400

    Base; Snake; Hangman; Minesweeper added

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('snake-game').addEventListener('click', function() {
+    alert('Snake Game Placeholder');
+});
+
+document.getElementById('minesweeper-game').addEventListener('click', function() {
+    alert('Minesweeper Game Placeholder');
+});
+
+document.getElementById('hangman-game').addEventListener('click', function() {
+    alert('Hangman Game Placeholder');
+});
